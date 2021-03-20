d3a82de8c559400aa3d6d8460239889c45c68dd0
Max Holman <max@holmn.com>
2021-03-20 23:48:40 +0800

    chore: initial commit

---
+++ lib/esbuild/esbuild-plugin-external-wildcard.ts
@@ -0,0 +1,20 @@
+import { Plugin } from 'esbuild';
+
+const pluginName = 'externals-regexp';
+
+export function externalsRegExpPlugin({
+  externals,
+}: {
+  externals: RegExp[];
+}): Plugin {
+  return {
+    name: pluginName,
+    setup(build) {
+      externals.forEach((filter) => {
+        build.onResolve({ filter }, (args) => {
+          return { ...args, external: true };
+        });
+      });
+    },
+  };
+}
---
+++ lib/types.ts
@@ -0,0 +1,35 @@
+// all optional because they might be on command like
+export interface BirudaConfigFileProperties {
+  entryPoints?: string[];
+  verbose?: boolean;
+  outDir?: string;
+  platform?: string;
+  externals?: string[];
+  forceInclude?: string[];
+  ignorePackages?: string[];
+  archiveFormat?: 'zip' | 'tar';
+  sourceMapSupport?: boolean;
+}
+
+export interface BirudaCliArguments {
+  config?: string;
+  verbose?: boolean;
+  output?: string;
+  // baseDir?: string;
+  entrypoint?: string[];
+  archiveFormat?: string;
+  forceInclude?: string[];
+  sourceMapSupport?: boolean;
+}
+
+export interface BirudaBuildOptions {
+  verbose?: boolean;
+  outDir: string;
+  entryPoint: string;
+  // baseDir: string;
+  platform: string;
+  externals?: (string | RegExp)[];
+  ignorePackages?: (string | RegExp)[];
+  forceInclude?: string[];
+  sourceMapSupport?: boolean;
+}
