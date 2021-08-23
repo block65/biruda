4bfb25b2166d47fede9ac104e02eeb2b1c5a81bd
Max Holman <max@holmn.com>
2021-08-23 20:41:17 +0800

    chore: new logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,12 +1,58 @@
-import createLogger from 'pino';
-import supportsColor from 'supports-color';
+import { createLogger, Logger } from '@block65/logger';
+import chalk from 'chalk';
+import * as util from 'util';
+
+interface LogDescriptor {
+  level: number;
+  msg: string;
+  time: number;
+  pid: number;
+  hostname: string;
+
+  [key: string]: unknown;
+}
+
+function formatLevel(level: number) {
+  switch (level) {
+    case 60: //fatal
+      return chalk.whiteBright.bgRed.bold('FATAL');
+    case 50: //error
+      return chalk.red('ERROR');
+    case 40: // warn
+      return chalk.yellow('WARN');
+    case 30: //info
+      return chalk.blue('INFO');
+    case 20: //debug
+      return chalk.green('DEBUG');
+    case 10: //trace
+      return chalk.gray('TRACE');
+  }
+}
 
 export const logger = createLogger({
-  // level: 'trace',
-  prettyPrint: {
-    colorize: !!supportsColor.stdout,
-    translateTime: true,
-    ignore: 'hostname,pid,time',
+  prettyPrint: {},
+  prettifier(thisArg: Logger, options: unknown) {
+    return (log: LogDescriptor) => {
+      const { level, msg, time, hostname, pid, ...rest } = log;
+
+      if (msg.startsWith('pino.final with prettyPrint')) {
+        return;
+      }
+
+      const formattedRest =
+        Object.keys(rest).length > 0
+          ? util.inspect(rest, {
+              colors: true,
+            })
+          : rest;
+
+      process.stdout.write(
+        `${formatLevel(level)}: ${new Date(
+          time,
+        ).toJSON()} ${msg}${formattedRest}\n`,
+      );
+      return true;
+    };
   },
 });
 
